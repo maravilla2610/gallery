08f988dcf0fec4aea0bf4e46f537e06171c319dd
Santiago <santiago.villavicencioc@gmail.com>
2025-10-15 18:15:11 -0600

    new callback url env

--- components/login-form-demo.tsx
+++ components/login-form-demo.tsx
@@ -22,7 +22,7 @@ export default function LoginFormDemo() {
     const { error } = await supabase.auth.signInWithOtp({
       email,
       options: {
-        emailRedirectTo: `${window.location.origin}/auth/callback`,
+        emailRedirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
       },
     });
 
